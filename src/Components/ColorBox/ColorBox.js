395ed3d34934ff267ce11b39f31a8fa7fdc4e771
GunMoLee <rjsahdia3@gmail.com>
2019-08-19 16:45:39 +0900

    Feature/color box (#2)
    * Add Library_ CopyToClipBoard
    * ColorBox Animation, CSS 작업

--- src/Components/ColorBox/ColorBox.js
+++ src/Components/ColorBox/ColorBox.js
@@ -1,13 +1,41 @@
 import React, { Component } from "react";
+import CopyToClipBoard from "react-copy-to-clipboard";
 import "./colorbox.css";
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { copied: false };
+  }
+
+  changeCopyState = () => {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 800);
+    });
+  };
+
   render() {
+    const { name, background } = this.props;
+    const { copied } = this.state;
+
     return (
-      <div className="ColorBox" style={{ background: this.props.background }}>
-        <span>{this.props.name}</span>
-        <span>MORE</span>
-      </div>
+      <CopyToClipBoard text={background} onCopy={this.changeCopyState}>
+        <div className="ColorBox" style={{ background }}>
+          {/* copy-container를 transform으로 커지게 하면 글자들도 다 커짐... */}
+          <div className={`copy-overlay ${copied && "show"}`} style={{ background }} />
+          <div className={`copy-message ${copied && "show"}`}>
+            <h1>copied!</h1>
+            <p>{this.props.background}</p>
+          </div>
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
+          </div>
+          <span className="see-more">More</span>
+        </div>
+      </CopyToClipBoard>
     );
   }
 }
