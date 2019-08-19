92b1e7a13643b355c227c4947a0786d8786c1f4a
Georgee-lee <rjsahdia3@gmail.com>
2019-08-20 08:39:50 +0900

    add font, styling navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ class Navbar extends Component {
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href="#">reactColorPicker</a>
+          <a href="#">reactcolorpicker</a>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>
