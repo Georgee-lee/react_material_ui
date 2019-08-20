34e68fc3d9aa93dee45ce188fe54da371f7a7641
Georgee-lee <rjsahdia3@gmail.com>
2019-08-20 09:50:29 +0900

    make little change modal

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 import "./navbar.css";
+import { IconButton } from "@material-ui/core";
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = { format: "hex" };
+    this.state = { format: "hex", open: false };
   }
 
   handleChange = e => {
-    this.setState({ format: e.target.value });
+    this.setState({ format: e.target.value, open: true });
     this.props.handleChange(e.target.value);
+  };
+
+  closeModal = () => {
+    this.setState({ open: false });
   };
 
   render() {
@@ -44,6 +51,30 @@ class Navbar extends Component {
             <MenuItem value="rgba">RGBA - rgba(255, 255, 255, 0.5)</MenuItem>
           </Select>
         </div>
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={this.state.open}
+          autoHideDuration={2000}
+          message={
+            <span id="change-message">
+              Format Changed to {format.toUpperCase()}
+            </span>
+          }
+          ContentProps={{
+            "aria-describedby": "change-message"
+          }}
+          onClose={this.closeModal}
+          action={[
+            <IconButton
+              onClick={this.closeModal}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
       </header>
     );
   }
